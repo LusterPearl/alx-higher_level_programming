a8b2aaf2f5b5aa6116d59cdb834f2138f151ab5c
LusterPearl <Salamioluwatosin90@gmail.com>
2023-09-07 10:17:52 -0700

    updated

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
-total_sum = 0
+if __name__ == "__main__":
+    total_sum = 0
 for i in range(len(sys.argv) - 1):
     total += int(sys.argv[i + 1])
     print("{}".format(total))
--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import hidden_4
-all_names = dir(hidden_4)
+if __name__ == "__main__":
+    all_names = dir(hidden_4)
 for names in all_names:
     if name[:2] != '__':
         print(name)
