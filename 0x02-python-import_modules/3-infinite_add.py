fb2576a8bc4f92cecac06ef1ee3930c8c45b4074
LusterPearl <Salamioluwatosin90@gmail.com>
2023-09-07 12:52:45 -0700

    update

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-import sys
-
 if __name__ == "__main__":
-    total_sum = 0
-for i in range(len(sys.argv) - 1):
-    total += int(sys.argv[i + 1])
-    print("{}".format(total))
+
+    import sys
+
+    total = 0
+    for i in range(len(sys.argv) - 1):
+        total += int(sys.argv[i + 1])
+        print("{}".format(total))
