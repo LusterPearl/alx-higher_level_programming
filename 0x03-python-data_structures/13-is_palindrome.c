5520128f8dcb9844612ce45653aaef16bc70bc33
LusterPearl <Salamioluwatosin90@gmail.com>
2023-09-10 06:29:12 -0700

    update

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,13 +1,7 @@
-/*
- * File: 13-is_palindrome.c
- * Auth: Mbah Nkemdinma
- */
-
 #include "lists.h"
 
-/*int is_palindrome(listint_t **head);
 int is_palindrome(listint_t **head);
-*/
+int is_palindrome(listint_t **head);
 
 /**
  * is_palindrome - checking if the singly linked list is palindrome
