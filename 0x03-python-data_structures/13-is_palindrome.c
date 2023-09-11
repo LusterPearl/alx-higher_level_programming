9e583d44c4b6fbc7f98c53b974a55fa47cfcf845
LusterPearl <Salamioluwatosin90@gmail.com>
2023-09-11 02:13:34 -0700

    updated

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -24,7 +24,6 @@ int is_palindrome(listint_t **head)
 	listint_t *fast = *head;
 	listint_t *prev = NULL;
 	listint_t *tmp;
-
 	/* use the slow and fast pointer approach to find the middle */
 	while (fast != NULL && fast->next != NULL)
 	{
@@ -55,53 +54,3 @@ int is_palindrome(listint_t **head)
 	}
 	return (1);
 }
-
-/**
- * main - main
- *
- * Return: 0
- */
-/*int main(void)
-{*/
-	/*create a sample linked list */
-	/*listint_t *head = NULL;
-	listint_t *current;*/
-
-	/* populated the linked list */
-	/*int values[] = {1, 2, 3, 3, 2, 1};
-
-	for (int i = 0; i < 6; i++)
-	{
-		listint_t *new_node = malloc(sizeof(listint_t));
-
-		if (new_node == NULL)
-		{
-			perror("Failed to alllocate memory");
-			return (-1);
-		}
-		new_node->n = values[i];
-		new_node->next = head;
-		head = new_node;
-	}*/
-
-	/* check if the linked list is a palindrome */
-	/*int result = is_palindrome(&head);
-
-	if (result)
-	{
-		printf("The linked list is a palindrome.\n");
-	}
-	else
-	{
-		printf("The linked list is not a palindrome.\n");
-	}
-
-	while (head != NULL)
-	{
-		current = head;
-		head = head->next;
-		free(current);
-	}
-	return (0);
-}*/
-
