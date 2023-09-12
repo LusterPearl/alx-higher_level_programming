6ba9b4e968b432bdb51ddbe6a90107e875c8466b
LusterPearl <Salamioluwatosin90@gmail.com>
2023-09-12 02:34:51 -0700

    updated

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -11,35 +11,29 @@
  */
 int is_palindrome(listint_t **head)
 {
+	listint_t *slow, *fast, *prev, *tmp;
+
 	if (*head == NULL || (*head)->next == NULL)
 	{
 		/* a list with a single element */
 		return (1);
 	}
 
-	listint_t *slow = *head;
-	listint_t *fast = *head;
-	listint_t *prev = NULL;
-	listint_t *tmp;
-	/* use the slow and fast pointer approach to find the middle */
+	slow = *head;
+	fast = *head;
+	prev = NULL;
+
+	/*use the slow and fast pointer approach */
 	while (fast != NULL && fast->next != NULL)
 	{
 		fast = fast->next->next;
-
-		/* reverse the first half of the list */
+		/*reverse the frist half of the list */
 		tmp = slow->next;
 		slow->next = prev;
 		prev = slow;
 		slow = tmp;
 	}
-
-	/* if the list has an odd number skips */
-	if (fast != NULL)
-	{
-		slow = slow->next;
-	}
-
-	/* compare the first half with the second half */
+	/* comapare the first half with the second half */
 	while (prev != NULL && slow != NULL)
 	{
 		if (prev->n != slow->n)
