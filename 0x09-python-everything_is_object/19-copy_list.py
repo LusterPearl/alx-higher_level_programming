3bf55c9d29f2dda04e525b6752b14e99fa8e9359
LusterPearl <Salamioluwatosin90@gmail.com>
2023-10-03 13:29:50 -0700

    updated

--- 0x09-python-everything_is_object/19-copy_list.py
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -1,12 +1,2 @@
-#!/usr/bin/python3
-""" Define a function name copy_list that l as input. """
-
-
 def copy_list(input_list):
-    """
-    Defines a copy(clone) list
-
-    Returns:
-    return an empty list
-    """
     return input_list[:]
