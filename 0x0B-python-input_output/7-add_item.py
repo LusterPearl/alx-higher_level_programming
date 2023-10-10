c9586c0cdf566f82a00fd0564089ec6f28ce398e
LusterPearl <Salamioluwatosin90@gmail.com>
2023-10-10 14:44:46 -0700

    updates

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,7 +2,6 @@
 """ A function that add all arguments to the script """
 
 import sys
-import os import path
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
