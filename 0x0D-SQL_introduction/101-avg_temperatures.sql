c555688783ad5382cb3e8ae5a834f8c684202610
LusterPearl <salamioluwatosin90@gmail.com>
2023-11-14 12:17:50 -0800

    added

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,5 +1,5 @@
 -- Import table dump into hbtn_0c_0 database and display average temperature by city.
-SELECT city, AVG(temperature) AS`avg_temp`
+SELECT city, AVG(`value`) AS`avg_temp`
 FROM `temperatures`
 GROUP BY `city`
 ORDER BY `avg_temp` DESC;
--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,6 +1,6 @@
 -- Display the top 3 cities' temperature during July and August ordered by temperature (descending).
 
-SELECT `city`, AVG(`temperature`) AS `avg_temp`
+SELECT `city`, AVG(`value`) AS `avg_temp`
 FROM `temperatures`
 WHERE MONTH(date) IN (7, 8)
 GROUP BY `city`
