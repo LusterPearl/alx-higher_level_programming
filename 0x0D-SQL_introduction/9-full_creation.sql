b0845fb87157a4a43cf71ec110e08e41e46f9b59
LusterPearl <salamioluwatosin90@gmail.com>
2023-11-14 11:29:25 -0800

    8 and 9

--- 0x0D-SQL_introduction/9-full_creation.sql
+++ 0x0D-SQL_introduction/9-full_creation.sql
@@ -1,11 +1,11 @@
 -- Script that creates a table second_table and adds multiple rows.
-CREATE TABLE IF NOT EXISTS  hbtn_0c_0.second_table (
+CREATE TABLE IF NOT EXISTS second_table (
 	id INT,
 	name VARCHAR(256),
 	score INT
 );
 
-INSERT INTO hbtn_0c_0.second_table (id, name, score) VALUES
+INSERT INTO second_table (id, name, score) VALUES
 	(1, 'john', 10),
 	(2, 'Alex', 3),
 	(3, 'Bob', 14),
