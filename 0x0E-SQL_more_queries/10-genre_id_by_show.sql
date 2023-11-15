f474166dc60823978adb6b9a0bf0da393e4940f7
LusterPearl <salamioluwatosin90@gmail.com>
2023-11-15 14:00:01 -0800

    updated

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,5 +1,5 @@
 -- Selecting shows with at least one genre linked
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_shows
-JOIN tv_show_genres ON tv_shows.id = tv_show_genres.tv_show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id;
+SELECT a.title, d.genre_id
+FROM tv_shows AS a
+INNER JOIN tv_show_genres AS d ON a.id = d.show_id
+ORDER BY a.title, d.genre_id;
