aa457ea0e2058006b69540e04bb81680c7cb9a16
LusterPearl <salamioluwatosin90@gmail.com>
2023-11-15 13:47:15 -0800

    updated

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- Selecting shows by their rating sum
 SELECT tv_shows.title, SUM(rating) AS rating_sum
 FROM tv_shows
-LEFT JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.tv_show_id
+INNER JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.tv_show_id
 GROUP BY tv_shows.id
-ORDER BY rating_sum DESC, tv_shows.title ASC;
+ORDER BY rating_sum DESC;
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,8 +1,7 @@
 -- Selecting genres by their rating sum
 SELECT tv_genres.name, SUM(tv_shows_rate.rating) AS rating_sum
 FROM tv_genres
-LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
-LEFT JOIN tv_shows ON tv_show_genres.tv_show_id = tv_shows.id
-LEFT JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.tv_show_id
+INNER JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows_rate ON tv_shows.id = tv_shows_rate.tv_show_id
 GROUP BY tv_genres.id
-ORDER BY rating_sum DESC, tv_genres.name ASC;
+ORDER BY rating_sum DESC;
