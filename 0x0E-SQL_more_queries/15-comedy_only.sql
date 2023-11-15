ac013f3f6fff06bbf7dbbb990c110be624b0ce03
LusterPearl <salamioluwatosin90@gmail.com>
2023-11-15 14:53:21 -0800

    done

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,7 +1,7 @@
 -- Selecting show titles for Comedy genre and displaying the result
-SELECT m.title
-FROM tv_shows AS m
-INNER JOIN tv_show_genres AS d ON m.id = d.show_id
-INNER JOIN tv_genres AS f ON f.id = d.genres.id
-WHERE f.name = 'Comedy'
-ORDER BY m.title;
+SELECT t.title
+FROM tv_shows AS t
+INNER JOIN tv_show_genres AS s ON t.id = s.show_id
+INNER JOIN tv_genres AS g ON g.id = s.genre.id
+WHERE g.name = 'Comedy'
+ORDER BY t.title;
