aca9b2dae3bf13a876617dfbaada1545349a26ba
LusterPearl <salamioluwatosin90@gmail.com>
2023-11-15 14:45:12 -0800

    done

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -3,4 +3,4 @@ SELECT title, SUM(rate) AS rating
 FROM tv_shows AS d
 INNER JOIN tv_show_ratings AS f ON d.id = f.show_id
 GROUP BY title
-ORDER BY rating DESC;
+ORDER BY rating;
--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -4,4 +4,4 @@ FROM tv_genres AS d
 INNER JOIN tv_show_genres AS e ON e.genre.id = d.id
 INNER JOIN tv_show_ratings AS f ON f.show.id = e.show_id
 GROUP BY name
-ORDER BY rating DESC;
+ORDER BY rating;
