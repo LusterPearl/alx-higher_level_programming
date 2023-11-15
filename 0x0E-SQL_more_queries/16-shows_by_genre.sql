2a06c8c382e89c474f40a0285dff5cd1741a7477
LusterPearl <salamioluwatosin90@gmail.com>
2023-11-15 14:49:58 -0800

    done

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,5 +2,5 @@
 SELECT a.title, b.name
 FROM tv_shows AS a
 LEFT JOIN tv_show_genres AS d ON a.id = d.show_id
-LEFT JOIN tv_genres AS c ON d.genre_id = b.id
+LEFT JOIN tv_genres AS b ON d.genre_id = b.id
 ORDER BY a.title, b.name;
