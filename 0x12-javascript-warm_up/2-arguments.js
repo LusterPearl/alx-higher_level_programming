3a022440b832581e4ef9548fa9abceda3579061e
LusterPearl <Salamioluwatosin90@gmail.com>
2023-12-11 11:54:22 -0800

    1 and 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -4,7 +4,7 @@ const argcount = process.argv.length - 2;
 
 if (argcount === 0) {
   console.log('No argument');
-} else if (argcount === 3) {
+} else if (argcount === 1) {
   console.log('Argument found');
 } else {
   console.log('Arguments found');
