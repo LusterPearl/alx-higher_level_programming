01bba8f95eeb98a05a993bb8053ff39bbb61022a
LusterPearl <Salamioluwatosin90@gmail.com>
2023-12-17 13:42:40 -0800

    done

--- 0x0F-python-object_relational_mapping/relationship_city.py
+++ 0x0F-python-object_relational_mapping/relationship_city.py
@@ -4,7 +4,7 @@ Module containing the class definition of a City
 """
 
 from sqlalchemy import Column, Integer, String, ForeignKey
-from model_state import Base, State
+from relationship_state import Base, State
 
 
 class City(Base):
@@ -12,6 +12,7 @@ class City(Base):
     City class that inherits from Base
     """
     __tablename__ = 'cities'
+
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,7 +5,7 @@ Module containing the class definition of a State
 
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from model_city import Base, City
+from relationship_city import Base, City
 
 
 class State(Base):
@@ -13,6 +13,7 @@ class State(Base):
     State class that inherits from Base
     """
     __tablename__ = 'states'
+
     id = Column(Integer, primary_key=True, autoincrement=True, nullable=False)
     name = Column(String(256), nullable=False)
     cities = relationship("City", backref="state",
