9db6b4d036e1563772ceddd592d761ac704b5e3c
LusterPearl <salamioluwatosin90@gmail.com>
2024-01-05 13:31:55 -0800

    reslove

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
 # This script sends a JSON POST request to the specified URL with the contents of a file and displays the body of the response
-URL=$1
-FILE=$2
-if [ -f "$FILE" ]; then
-    curl -sX POST -H "Content-Type: application/json" -d @"$FILE" "$URL"
-else
-    echo "File not found: $FILE"
-fi
+curl -sX POST -H "Content-Type: application/json" -d @"$2" "$1"
