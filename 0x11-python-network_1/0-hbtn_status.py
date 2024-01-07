ee79f5a143cbe7374e7837897b897d96215b0fe4
LusterPearl <salamioluwatosin90@gmail.com>
2024-01-07 13:01:22 -0800

    task6

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,9 +7,9 @@ import urllib.request
 
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-    with urllib.request.urlopen(url) as response:
-        body = response.read()
-        utf8_content = body.decode('utf-8')
+    with urlopen(url) as response:
+        page = response.read()
+        headers = response.info()
         print("Body response:")
         print(f"    - type: {type(body)}")
         print(f"    - content: {body}")
---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+takes in a URL and an email address, sends a POST
+"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    email = sys.argv[2]
+
+    payload = {'email': email}
+    response = requests.post(url, data=payload)
+
+    print(f"Your email is: {email}")
+    print(response.text)
