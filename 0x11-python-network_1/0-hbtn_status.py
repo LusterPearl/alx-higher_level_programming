986f8affc527a777466f8d0e89a07c60af97de64
LusterPearl <salamioluwatosin90@gmail.com>
2024-01-07 11:33:19 -0800

    readmeandtask0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+takes all https alx-intranet status and tabulates response
+"""
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        utf8_content = body.decode('utf-8')
+
+        print("Body response:")
+        print(f"    - type: {type(body)}")
+        print(f"    - content: {body}")
+        print(f"    - utf8 content: {utf8_content}")
