7f3c7f8e7387aacabe0ee0887a672571871ac776
LusterPearl <salamioluwatosin90@gmail.com>
2024-01-07 11:36:22 -0800

    task0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -4,13 +4,12 @@ takes all https alx-intranet status and tabulates response
 """
 import urllib.request
 
+
 if __name__ == "__main__":
     url = "https://alx-intranet.hbtn.io/status"
-
     with urllib.request.urlopen(url) as response:
         body = response.read()
         utf8_content = body.decode('utf-8')
-
         print("Body response:")
         print(f"    - type: {type(body)}")
         print(f"    - content: {body}")
