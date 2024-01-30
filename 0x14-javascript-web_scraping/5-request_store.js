fa76480802f4c08b7022706902adf9453e03b8d7
LusterPearl <Salamioluwatosin90@gmail.com>
2024-01-30 08:12:11 -0800

    okay

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,4 +3,29 @@ const fs = require('fs');
 const request = require('request');
 
 // Get the URL and file path from the command line arguments
-const url =
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+// Check if both URL and file path are provided
+if (!url || !filePath) {
+  console.error('Usage: ./5-request_store.js https://jsonplaceholder.typicode.com/todos <file_path>');
+  process.exit(1);
+}
+
+// Make a request to te specified URL
+request(url, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+    process.exit(1);
+  }
+
+  // Write the body response to the specified file path
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.error('Error writing to file:', err);
+      process.exit(1);
+    }
+
+    console.log(`Body response stored in ${filePath}`);
+  });
+});
