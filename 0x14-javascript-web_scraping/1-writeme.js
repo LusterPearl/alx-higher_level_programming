5c6529ddd9558a6a5b22f0c454145c3e0ee28aaa
LusterPearl <Salamioluwatosin90@gmail.com>
2024-01-30 06:18:47 -0800

    task22

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -7,7 +7,7 @@ const contentToWrite = process.argv[3];
 
 // Check if both file path and content are provided
 if (!filePath || !contentToWrite) {
-  console.error('Usage: ./1-writeme.js <file-path> <string-to-write>');
+  console.error('Usage: ./1-writeme.js my_file.txt "Python is cool"');
   process.exit(1);
 }
 
